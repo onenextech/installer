353c699c9288432f561acacb32c5335ab73eac07
Nay Thu Khant <naythukhant644@gmail.com>
2023-02-20 15:35:07 +0630

    initial commit

--- onenex.rb
+++ onenex.rb
@@ -2,7 +2,7 @@ class Onenex < Formula
   desc "Command-line interface install starter kits from github.com/onenextech"
   homepage "https://github.com/onenextech/installer"
   url "https://github.com/onenextech/installer/archive/v1.0.0.tar.gz"
-  sha256 "22d5c5c8405d8906c341f29950faf8af1758efae5385fcc3c3889358b03cfe43"
+  sha256 "81530ef9813498f363f928c881aa070c1e799d4753749774481ac5192a5acc50"
 
   def install
     bin.install "onenex"
