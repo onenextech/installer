d2eba18d650bc30eda545c8293bc53169ec2ee5d
Nay Thu Khant <naythukhant644@gmail.com>
2023-02-20 15:56:55 +0630

    initial commit

--- onenex.rb
+++ onenex.rb
@@ -2,7 +2,7 @@ class Onenex < Formula
   desc "A command-line tool to create starter kits for Laravel, Swift, Kotlin, and Vue"
   homepage "https://github.com/onenex/installer"
   url "https://github.com/onenextech/installer/blob/main/onenex_v1.0.0.tar.gz"
-  sha256 "13020cfe5ae9b3b7703e338b24b636cdf3b8e62847083babdab29f8be42849e6"
+  sha256 "d1392af094bb6c540b495baed34b14bcbff98437e1260b6dff4b817f1df6920c"
   license "MIT"
 
   def install
